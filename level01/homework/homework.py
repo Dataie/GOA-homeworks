2a9cfd09587aa2aa626550e4a51250eb1f30d674
Dataie <datapkhakadze570@gmail.com>
2024-10-24 18:46:09 +0400

    commit

---
+++ level01/homework/homework.py
@@ -0,0 +1,9 @@
+name = "Data"
+surname = "Pkhakadze"
+age = 15
+height = 186
+school = "N64 Public School"
+interests = "sports and programming"
+print("Hello" + "," + " " + "my name is" + " " + name + " " + surname + "." + " " + "My age is" + " " + str(age) + "." + " " + "I am" + " " + str(height) + " " + "centimeters in height" + "."+ " " + "I study at" + " " + school + "." + " " + "My interests are" + " " + interests + ".") 
+
+
---
+++ level04/classwork/none.py
@@ -0,0 +1,56 @@
+name = "Data"
+surname = "Pkhakadze"
+age = 15
+num = 100
+height = 186.5
+random = 14.234
+
+print(name)
+print(surname)
+print(age)
+print(num)
+print(height)
+print(random)
+
+print(5 + 1)
+print(5 + 0)
+print(5 + 2)
+print(5 + 3)
+print(5 + 4)
+
+
+print(2 - 0)
+print(1 - 3)
+print(9 - 5)
+print(8 - 3)
+print(6 - 6)
+
+print(2 / 4)
+print(4 / 2)
+print(9 / 3) 
+print(10 / 2)
+print(10 / 5)
+
+
+
+print(10 % 3)
+print(6 % 4)
+print(4 % 2)
+print(5 % 5)
+print(100 % 78)
+
+print(2 * 2)
+print(2 * 1)
+print(2 * 3)
+print(2 * 4)
+print(2 * 5)
+
+print(2 ** 5)
+print(5 ** 2)
+print(9 ** 8)
+print(3 ** 7)
+print(3 ** 2)
+
+
+
+
