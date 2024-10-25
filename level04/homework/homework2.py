587326a83860a9dd8f9add88c11b7de095a48e89
Dataie <datapkhakadze570@gmail.com>
2024-10-25 21:40:34 +0400

    Delete level04 directory

--- level04/homework/homework2.py
+++
@@ -1,3 +0,0 @@
-celsius = 11
-fahrenheit = (celsius) * (9 / 5) + 32
-print(fahrenheit)
--- level04/homework/homework3.py
+++
@@ -1,8 +0,0 @@
-age = [18,31,48,49]
-sister1, sister2, mother, father = age
-print(sister1 + 20)
-print(sister2 + 20)
-print(mother +20)
-print(father +20)
-
-
