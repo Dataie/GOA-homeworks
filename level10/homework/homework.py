99e09c0de240a00a439abdabd55aebcdcd865d0d
Dataie <datapkhakadze570@gmail.com>
2024-11-13 22:13:02 +0400

    commit

---
+++ level10/homework/homework.py
@@ -0,0 +1,260 @@
+#>
+num1 = 1
+num2 = 2
+print(num2 > num1)
+
+num1 = 3
+num2 = 4
+print( num2 > num1)
+
+num1 = 5
+num2 = 6
+print(num2 > num1)
+
+num1 = 7
+num2 = 8
+print(num2 > num1)
+
+num1 = 2562
+num2 = 77547
+print(num2 > num1)
+
+num1 = 437.763
+num2 = 7433236
+print(num2 > num1)
+
+num1 = -4667346734673
+num2 = 1
+print(num2 > num1)
+
+num1 = 4.3456543655
+num2 = 35
+print(num2 > num1)
+
+num1 = 0.7465
+num2 = 1.0
+print(num2 > num1)
+
+num1 = 576
+num2 = 6643
+print(num2 > num1)
+
+
+#<
+num1 = 1324
+num2 = 213
+print(num1 < num2)
+
+num1 = 2134.432
+num2 = 231
+print(num1 < num2)
+
+num1 = 549432
+num2 = 23.54
+print(num1 < num2)
+
+num1 = 324
+num2 = -45832458
+print(num1 < num2)
+
+num1 = 5656
+num2 = 653.224
+print(num1 < num2)
+
+num1 = 963
+num2 = -3456.349875865
+print(num2 < num1)
+
+num1 = 6
+num2 = 9876
+print(num1 < num2)
+
+num1 = 489
+num2 = 318.324
+print(num1 < num2)
+
+num1 = 8.94
+num2 = 2
+print(num2 < num1)
+
+
+#>=
+num1 = 235
+num2 = -3286
+print(num1 >= num2)
+
+num1 = 675
+num2 = 346376
+print(num1 >= num2)
+
+num1 = 9457958749739
+num2 = 7890286
+print(num1 >= num2)
+
+num1 = 756754
+num2 = -3286
+print(num1 >= num2)
+
+num1 = 2355456
+num2 = -3286
+print(num1 >= num2)
+
+num1 = 0.235
+num2 = -2865464
+print(num1 >= num2)
+
+num1 = 2
+num2 = -32865474
+print(num1 >= num2)
+
+num1 = 25
+num2 = -86
+print(num1 >= num2)
+
+num1 = 35
+num2 = -36
+print(num1 >= num2)
+
+num1 = 5
+num2 = -6
+print(num1 >= num2)
+
+
+#<=
+num1 = 5
+num2 = 6
+print(num1 <= num2)
+
+num1 = 36
+num2 = 34
+print(num1 <= num2)
+
+num1 = 3332
+num2 = 654
+print(num1 <= num2)
+
+num1 = -2.974
+num2 = 0.55
+print(num1 <= num2)
+
+num1 = 3567
+num2 = 764
+print(num1 <= num2)
+
+num1 = 34
+num2 = 64345
+print(num1 <= num2)
+
+num1 = 435646
+num2 = 86
+print(num1 <= num2)
+
+num1 = 3445
+num2 = 45
+print(num1 <= num2)
+
+num1 = 7979797979
+num2 = 9797979797
+print(num1 <= num2)
+
+num1 = 76
+num2 = 9
+print(num1 <= num2)
+
+
+#!=
+num1 = 2
+num2 = 2
+print(num1 != num2)
+
+num1 = 4
+num2 = 8
+print(num1 != num2)
+
+num1 = 46
+num2 = 64
+print(num1 != num2)
+
+num1 = 46643
+num2 = 75
+print(num1 != num2)
+
+num1 = 645645645
+num2 = 645645645
+print(num1 != num2)
+
+num1 = 3334544
+num2 = 433
+print(num1 != num2)
+
+num1 = -49464
+num2 = 0.54939
+print(num1 != num2)
+
+num1 = 235
+num2 = 643 
+print(num1 != num2)
+
+num1 = 354
+num2 = 45323 
+print(num1 != num2)
+
+num1 = 7548
+num2 = 3543
+print(num1 != num2)
+
+
+#==
+num1 = 45
+num2 = 654
+print(num1 == num2)
+
+num1 = 64564
+num2 = 56
+print(num1 == num2)
+
+num1 = 756
+num2 = 87
+print(num1 == num2)
+
+num1 = -567
+num2 = 567
+print(num1 == num2)
+
+num1 = 64
+num2 = 56
+print(num1 == num2)
+
+num1 = 45
+num2 = 45
+print(num1 == num2)
+
+num1 = 65
+num2 = 765
+print(num1 == num2)
+
+num1 = 4567
+num2 = 564
+print(num1 == num2)
+
+num1 = 4567
+num2 = 4567
+print(num1 == num2)
+
+num1 = 765
+num2 = 789
+print(num1 == num2)
+
+
+age = int(input("Enter your age here: "))
+age_in_10_years = (age + 10)
+print("Your age in ten years will be " + str(age_in_10_years))
+
+
+person1 = int(input("Enter your age here: "))
+person2 = int(input("Enter your age here: "))
+person3 = int(input("Enter your age here: "))
+mean = ((person1 + person2 + person3)/3 )
+print(mean)
+
+
