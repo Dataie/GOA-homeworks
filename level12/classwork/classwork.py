2e4d66c5d505bd5272e653857c7774bc7f89d352
Dataie <datapkhakadze570@gmail.com>
2024-11-13 22:15:22 +0400

    commit

---
+++ level12/classwork/classwork.py
@@ -0,0 +1,15 @@
+print(True or True)#True
+print(True or False)#True
+print(False or True)#True
+print(False or False)#False
+
+print(True and True)#True
+print(True and False)#False
+print(False and True)#False
+print(False and False)#False
+
+
+
+user1 = int(input("Enter number: "))
+user2 = int(input("Enter number: "))
+print(user1 > 30 and user2 < 40)
