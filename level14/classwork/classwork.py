d283e00fef89a270d60f592937ca7d4229222faa
Dataie <datapkhakadze570@gmail.com>
2024-11-26 19:44:22 +0400

    commit

---
+++ level14/classwork/classwork.py
@@ -0,0 +1,16 @@
+for i in range(20):
+    print("Data Pkhakadze")
+
+num1 = int(input("Enter number here: "))
+num2 = int(input("Enter number here: "))
+
+for i in range(num1, num2):
+   print(i**2)
+
+num1, num2 = 1, 2
+
+num1 = int(input("Enter number: "))
+
+for i in range(1, num1):
+    print(i**2)
+
---
+++ level14/homework/homework.py
@@ -0,0 +1,53 @@
+first_range = range(20, 50)
+second_range = range(100, 150)
+third_range = range(20 , 50 , 2)
+fourth_range = range(11 , 31 , 2)
+
+for i in range(10):
+    print("Data Pkhakadze")
+
+for i in range(10, 30):
+    print(i / 2)
+
+for i in range(40, 60):
+    print(i ** 3)
+
+for i in range(5):
+    print(int(input("Enter your number here: ")))
+
+name = input("Enter your name here: ")
+for i in name: 
+    print(i)
+
+num1 = int(input("Enter your first number here: "))
+num2 = int(input("Enter your second number here: "))
+num3 = int(input("Enter your third number here: "))
+for i in range(num1, num2, num3):
+    print(i ** 2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
